9952217d97d5516e24358dd1ef0ca926c653b2b9
Khanh <ngokhanh809@gmail.com>
2023-11-02 17:05:03 -0700

    initial commit

---
+++ build.bat
@@ -0,0 +1,54 @@
+@echo off
+
+
+
+set "ARG1=%1"
+set "OUTDIRS=bin obj"
+
+set "CC=gcc"
+set "CCF=-Og -DDEBUG -Wall -Wextra -Wpedantic"
+set "LDF="
+set "LIBS="
+
+set "MSG="
+set "SRCS=src\main.c"
+set "OUTPUT=bin\pascal.exe"
+
+
+
+if "%ARG1%"=="clean" (
+    rmdir /q /s bin
+    rmdir /q /s obj
+
+    set "MSG=Removed build artifacts."
+) else (
+    if not exist bin\ mkdir bin
+    if not exist obj\ mkdir obj
+
+    setlocal enabledelayedexpansion
+    set "OBJS="
+    for %%F in (%SRCS%) do (
+        set CURRENT_OBJ=obj/%%~nF.o
+
+        :: compile each src file 
+        echo %CC% %CCF% -c %%F -o !CURRENT_OBJ!
+        %CC% %CCF% -c %%F -o !CURRENT_OBJ!
+
+        :: add obj file to list 
+        set "OBJS=!OBJS! !CURRENT_OBJ!"
+    )
+    :: linking 
+    echo %CC% %LDF% -o %OUTPUT% !OBJS! %LIBS%
+    %CC% %LDF% -o %OUTPUT% !OBJS! %LIBS%
+
+    :: cleanup
+    set "OBJS="
+
+    set "MSG=Build finished."
+)
+
+echo --------------------------------------
+echo %MSG%
+echo --------------------------------------
+
+
