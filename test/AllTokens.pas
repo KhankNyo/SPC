48c66e9ec24bf1f85564946f5fd6e4e2a4024666
Khanh <ngokhanh809@gmail.com>
2023-11-02 22:11:31 -0700

    added test with all keywords

---
+++ test/AllTokens.pas
@@ -0,0 +1,84 @@
+and
+array
+asm
+begin
+end
+break
+case 
+const
+constructor
+continue
+destructor
+div
+do 
+downto
+else 
+end
+false
+file
+for
+function
+goto
+if 
+implementation
+in 
+inline
+interface
+label
+mode
+nil
+not
+object
+of
+on 
+operator
+or 
+packed
+procedure
+program
+record
+repeat
+set
+shl
+shr
+string
+then
+true
+type
+(* 
+        this is a comment
+        that spans multiple lines
+*)
+{
+    this is also a comment that
+    spans multiple lines 
+}
+// this is a cpp comment
+// that does not span multiple lines
+
+unit
+until
+uses
+var
+while 
+with
+xor
+
+
+// symbols
++ - * / 
++= -= *= /=
+**
+= < > <>
+. , : ;
+[ ] ( ) 
+^ @ $ # & %
+
+1.23
+0x123
+123 
+'this is a string literal'
+'this is a string literal with a newline'#10
+ThisIsAnIdentifier
+
+
