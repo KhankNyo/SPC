1d0acfafadc54a716ffd5c362c29c1c29058838e
Khanh <ngokhanh809@gmail.com>
2023-11-05 13:15:27 -0800

    separated debug file from VM

--- build.bat
+++ build.bat
@@ -32,7 +32,7 @@ if "%ARG1%"=="cl" (
 set "MSG="
 set "SRCS=%SRCDIR%\PascalString.c %SRCDIR%\main.c %SRCDIR%\Pascal.c %SRCDIR%\Memory.c"
 set "SRCS=%SRCS% %SRCDIR%\Tokenizer.c %SRCDIR%\Parser.c %SRCDIR%\Ast.c"
-set "SRCS=%SRCS% %SRCDIR%\PVM\PVM.c %SRCDIR%\PVM\CodeChunk.c"
+set "SRCS=%SRCS% %SRCDIR%\PVM\Disassembler.c %SRCDIR%\PVM\PVM.c %SRCDIR%\PVM\CodeChunk.c"
 set "OUTPUT=%BINDIR%\pascal.exe"
 
 
---
+++ src/Include/PVM/Disassembler.h
@@ -0,0 +1,13 @@
+#ifndef PASCAL_VM_DISASSEMBLER_H
+#define PASCAL_VM_DISASSEMBLER_H
+
+
+
+#include "PVM.h"
+
+
+void PVMDisasm(FILE *f, const CodeChunk *Chunk);
+
+
+#endif /* PASCAL_VM_DISASSEMBLER_H */
+
