3bcf98a3541051c4eb2874b7248f18446b96d1b6
Khanh <ngokhanh809@gmail.com>
2023-11-07 10:03:38 -0800

    added PVM compiler

---
+++ src/Include/PVM/CodeGen.h
@@ -0,0 +1,15 @@
+#ifndef PASCAL_VM_CODEGEN_H
+#define PASCAL_VM_CODEGEN_H
+
+
+
+#include "CodeChunk.h"
+#include "Ast.h"
+
+
+CodeChunk PVMCompile(const PascalAst *Ast);
+
+
+
+#endif /* PASCAL_VM_CODEGEN_H */
+
