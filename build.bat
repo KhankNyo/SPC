ce936230287fa19ae019df639c1afa1fa392b2ce
Khanh <ngokhanh809@gmail.com>
2023-11-10 23:00:41 -0800

    Added Vartab.c

--- build.bat
+++ build.bat
@@ -31,7 +31,7 @@ if "%ARG1%"=="cl" (
 
 set "MSG="
 set "SRCS=%SRCDIR%\PascalString.c %SRCDIR%\main.c %SRCDIR%\Pascal.c %SRCDIR%\Memory.c"
-set "SRCS=%SRCS% %SRCDIR%\Tokenizer.c %SRCDIR%\Parser.c %SRCDIR%\Ast.c"
+set "SRCS=%SRCS% %SRCDIR%\Tokenizer.c %SRCDIR%\Parser.c %SRCDIR%\Ast.c %SRCDIR%\Vartab.c"
 set "SRCS=%SRCS% %SRCDIR%\PVM\Disassembler.c %SRCDIR%\PVM\PVM.c %SRCDIR%\PVM\CodeChunk.c %SRCDIR%\PVM\CodeGen.c"
 set "OUTPUT=%BINDIR%\pascal.exe"
 
